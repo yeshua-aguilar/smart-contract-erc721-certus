ea4df750150975d2a71a019639f1e6cea169ee36
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2025-05-17 07:25:20 -0500

    feat: agregar control de acceso al mint y mejorar pruebas

--- contracts/TestERC721.sol
+++ contracts/TestERC721.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestERC721 is ERC721URIStorage {
+contract TestERC721 is ERC721URIStorage, Ownable {
     uint256 private _tokenIds;
 
-    constructor() ERC721("TestERC721", "T721") {}
+    constructor() ERC721("TestERC721", "T721") Ownable(msg.sender) {}
 
-    function mint(address to, string memory _tokenURI) public {
+    function mint(address to, string memory _tokenURI) public onlyOwner {
         _tokenIds++;
         uint256 newItemId = _tokenIds;
         _mint(to, newItemId);
--- test/TestERC721.test.js
+++ test/TestERC721.test.js
@@ -4,9 +4,10 @@ const { ethers } = require("hardhat");
 describe("TestERC721", function () {
     let testERC721;
     let owner;
+    let user;
 
     beforeEach(async function () {
-        [owner] = await ethers.getSigners();
+        [owner, user] = await ethers.getSigners();
         const factory = await ethers.getContractFactory("TestERC721");
         testERC721 = await factory.deploy();
         await testERC721.waitForDeployment();
@@ -17,11 +18,18 @@ describe("TestERC721", function () {
         expect(await testERC721.symbol()).to.equal("T721");
     });
 
-    it("Debería crear un NFT", async function () {
+    it("Debería crear un NFT cuando lo llama el propietario", async function () {
         const tokenURI = "https://example.com/tokenURI";
         await testERC721.mint(owner.address, tokenURI);
         expect(await testERC721.ownerOf(1)).to.equal(owner.address);
         expect(await testERC721.tokenURI(1)).to.equal(tokenURI);
+    });
+
+    it("Debería fallar al crear un NFT cuando lo llama una cuenta no autorizada", async function () {
+        const tokenURI = "https://example.com/tokenURI";
+        await expect(
+            testERC721.connect(user).mint(user.address, tokenURI)
+        ).to.be.revertedWithCustomError(testERC721, "OwnableUnauthorizedAccount");
     });
 
     it("Debería fallar al obtener el propietario de un NFT inexistente", async function () {
